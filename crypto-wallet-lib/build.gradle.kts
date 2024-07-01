bf2f95728a40800bc12083e771d8648edb46e0a7
Thanh Phat <1578134+thanhphat@users.noreply.github.com>
2024-07-01 10:31:34 +0700

    Update version

--- crypto-wallet-lib/build.gradle.kts
+++ crypto-wallet-lib/build.gradle.kts
@@ -122,7 +122,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.innfocus",
         artifactId = "crypto-wallet-lib",
-        version = "1.0.3-alpha.6"
+        version = "1.0.3-alpha.7"
     )
 
     // Configure POM metadata for the published artifact
