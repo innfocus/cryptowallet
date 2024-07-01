cc0813fb88e637033675c691721ab650b0f0b040
Thanh Phat <1578134+thanhphat@users.noreply.github.com>
2024-07-01 10:39:09 +0700

    Update auth token

--- crypto-wallet-lib/src/commonMain/kotlin/com/lybia/walletapp/services/WalletAppService.kt
+++ crypto-wallet-lib/src/commonMain/kotlin/com/lybia/walletapp/services/WalletAppService.kt
@@ -7,14 +7,26 @@ import com.lybia.walletapp.requests.SendEmailVerificationRequest
 import com.lybia.walletapp.responses.BaseApiResponse
 import com.lybia.walletapp.responses.CheckEmailVerificationResponse
 import io.ktor.client.call.body
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 
-class WalletAppService(private val serverUrl: String) {
+class WalletAppService(
+    private val serverUrl: String,
+    private var authToken: String?
+) {
     private val client = HttpClientService.INSTANCE.client
+
+    // Private helper function to add Authorization header if authToken exists
+    private fun HttpRequestBuilder.addAuthorizationHeader() {
+        authToken?.let { token ->
+            header("Authorization", "Bearer $token")
+        }
+    }
 
     suspend fun sendOtp(email: String): BaseApiResponse {
         val response: HttpResponse = client.post("$serverUrl/api/v2/verifications") {
@@ -41,6 +53,7 @@ class WalletAppService(private val serverUrl: String) {
         email: String?, name: String?
     ): BaseApiResponse {
         val response: HttpResponse = client.post("$serverUrl/api/v1/consultations") {
+            addAuthorizationHeader()
             contentType(ContentType.Application.Json)
             setBody(SendConsultationRequest(inquiry, email, name))
         }
