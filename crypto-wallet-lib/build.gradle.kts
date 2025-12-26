cd63523616e65f2b9bbf23cd5b665d23d2572bc3
@quanhuy <64772864+nqhuy2509@users.noreply.github.com>
2025-12-26 14:46:15 +0700

    bump version to 1.1.5 and update Bouncy Castle to 1.83

--- crypto-wallet-lib/build.gradle.kts
+++ crypto-wallet-lib/build.gradle.kts
@@ -130,7 +130,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.innfocus",
         artifactId = "crypto-wallet-lib",
-        version = "1.1.4"
+        version = "1.1.5"
     )
 
     // Configure POM metadata for the published artifact
