a522d6086ce13eba41172e98ac93fde3a4e9df0b
Thanh Phat <1578134+thanhphat@users.noreply.github.com>
2026-03-13 23:24:23 +0700

    add androidxLifecycle

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
     
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
