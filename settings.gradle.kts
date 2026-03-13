defbc4bcadecdabd5ae8bd50cdb23b40fe091b37
Thanh Phat <1578134+thanhphat@users.noreply.github.com>
2026-03-13 17:22:56 +0700

    Gradle toolchain

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,9 @@ pluginManagement {
         maven("https://jitpack.io")
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 
 dependencyResolutionManagement {
     repositories {
