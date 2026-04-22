eff61266be67e185203568631320f958d5bbc12a
@quanhuy <64772864+nqhuy2509@users.noreply.github.com>
2026-04-23 06:09:35 +0700

    Update Package.swift for v1.2.12

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "crypto-wallet-lib",
-         url: "https://github.com/innfocus/cryptowallet/releases/download/v1.2.11/crypto_wallet_lib.xcframework.zip",
-         checksum: "3d55d43c22b211066b22ea7e7909a292b5f3e456e8694d96654e9939a70a28c9"
+         url: "https://github.com/innfocus/cryptowallet/releases/download/v1.2.12/crypto_wallet_lib.xcframework.zip",
+         checksum: "7a3947afad17b06e035db2fdc13b02e58194b70b7951b8c5f4b59e8636435303"
       )
    ]
 )
